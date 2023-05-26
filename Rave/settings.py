c5d3975499b01e1f00daa81649b3a694c3aa62ea
frontedTee <adetunjitayo2019@gmail.com>
2023-05-25 14:52:52 -1200

    added railway config

--- Rave/settings.py
+++ Rave/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['rave-production.up.railway.app','127.0.0:1']
 
 
 # Application definition
